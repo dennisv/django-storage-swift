8eef605f387af41dbd7d76639f4cafa5f2afbecb
Dennis Vermeulen <blacktorn@gmail.com>
2013-08-29 20:12:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-storage-swift',
-      version='0.8',
+      version='0.9',
       description='OpenStack Swift storage backend for Django',
       url='http://github.com/wecreatepixels/django-storage-swift',
       author='Dennis Vermeulen',
