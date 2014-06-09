5168ff41c47312a307614e14ae669046dcf44237
Dennis Vermeulen <dennis@eyo.nl>
2014-06-10 00:39:37 +0200

    Version 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-storage-swift',
-      version='1.0',
+      version='1.1',
       description='OpenStack Swift storage backend for Django',
       url='http://github.com/wecreatepixels/django-storage-swift',
       author='Dennis Vermeulen',
