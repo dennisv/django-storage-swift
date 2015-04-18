c30599ad9ba1a2a74c9a28a9b5c7b3343e5037aa
Dennis Vermeulen <dennis@wecreatepixels.nl>
2015-04-18 15:50:37 +0200

    Bump version and add trove classifiers

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
 from setuptools import setup
 
-setup(name='django-storage-swift',
-      version='1.2.3',
-      description='OpenStack Swift storage backend for Django',
-      url='http://github.com/blacktorn/django-storage-swift',
-      author='Dennis Vermeulen',
-      author_email='blacktorn@gmail.com',
-      license='MIT',
-      packages=['swift'],
-      install_requires=[
-          'python-swiftclient>=1.4.0',
-          'python-keystoneclient>=0.2.3',
-          'six',
-      ],
-      zip_safe=False)
+setup(
+    name='django-storage-swift',
+    version='1.2.4',
+    description='OpenStack Swift storage backend for Django',
+    url='http://github.com/blacktorn/django-storage-swift',
+    author='Dennis Vermeulen',
+    author_email='blacktorn@gmail.com',
+    license='MIT',
+    packages=['swift'],
+    install_requires=[
+        'python-swiftclient>=1.4.0',
+        'python-keystoneclient>=0.2.3',
+        'six',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
