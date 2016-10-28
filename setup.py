b0a480b2764d4b5a83430fdbd902b1b11fbfae25
Einar Forselv <eforselv@gmail.com>
2016-10-28 12:28:56 +0200

    Working tests (#59)

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
---
+++ tests/test_test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class DummyTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_dummy(self):
+        pass
