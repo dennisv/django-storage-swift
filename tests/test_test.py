218e89af85fc4e8e5bdb52fb07876ff6637652fa
Einar Forselv <eforselv@gmail.com>
2016-10-28 15:30:45 +0200

    Tests: instantiate backends

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
+from .utils import FakeSwift
+from swift import storage
 
 
 class DummyTestCase(TestCase):
 
     def setUp(self):
-        pass
+        storage.swiftclient = FakeSwift
+        self.media_storage = storage.SwiftStorage()
+        self.static_storage = storage.StaticSwiftStorage()
 
     def test_dummy(self):
         pass
